759620972e6e38aca0aa41d3cfb402ef074adfaf
Sujon <abdur.sujon.dev@gmail.com>
2026-06-07 02:25:21 +0100

    custom built arraylist in progress£

---
+++ src/custom_ds/ArrayList.java
@@ -0,0 +1,72 @@
+import interfaces.List;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class ArrayList<E> implements List<E> {
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public void add(E element) {
+
+    }
+
+    @Override
+    public void addAll(Collection<E> collection) {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(E element) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
--- src/custom_ds/interfaces/List.java
+++ src/custom_ds/interfaces/List.java
@@ -1,4 +1,22 @@
 package interfaces;
 
-public interface List {
+import java.util.Collection;
+import java.util.Iterator;
+
+public interface List<E> extends Iterable<E>{
+    void add(int index, E element);
+    void add(E element); // add at the last index
+    void addAll(Collection<E> collection);
+    E get(int index);
+    E set(int index, E element);
+    E remove(int index);
+    boolean remove(E element);
+    void clear();
+    boolean contains(E element);
+    int indexOf(E element);
+    int size();
+    boolean isEmpty();
+    Iterator<E> iterator();
+    @Override
+    String toString();
 }
